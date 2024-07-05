976335407cda019562fbbe6691e5bfcc6e79a0cd
Mahmoud H. Alkahlout <hardak9@gmail.com>
2024-07-05 17:43:46 +0300

    renaming folders

--- models/User_m.php
+++
@@ -1,6 +0,0 @@
-<?php defined('__ROOT__') OR exit('No direct script access allowed');
-
-class User_m extends Model {
-	protected $_table_name = 'users';
-	protected $_order_by = 'name';
-}
--- views/home.php
+++
@@ -1,6 +0,0 @@
-<?php defined('__ROOT__') OR exit('No direct script access allowed'); ?>
-<div class="jumbotron">
-	<div class="container">
-	  <h1 class="display-3"><?php echo __SITE_NAME__; ?></h1>
-	</div>
-</div>
